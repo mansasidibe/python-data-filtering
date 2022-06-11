0bfb460aebec47b9abe2b477c529753c74660b8d
ronphobos <sidibearouna71@gmail.com>
2022-06-11 11:23:42 +0000

    analyse de données

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #%%
 from pathlib import Path
-
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 ## lecture de fichier csv
 pokemon = pd.read_csv(Path().joinpath('donnees', 'pokemon.csv'))
@@ -11,4 +12,6 @@ pokemon_show = pokemon.sample(5)
 
 ## somme des colonne null
 somme = pokemon.isnull().sum()
-print(somme)
+
+
+
