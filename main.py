75ea678233ecc2b5ac5378a4ebcd48d02a11d9a3
ronphobos <sidibearouna71@gmail.com>
2022-06-11 11:05:26 +0000

    analyse de données

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ import pandas as pd
 ## lecture de fichier csv
 pokemon = pd.read_csv(Path().joinpath('donnees', 'pokemon.csv'))
 
-print(pokemon)
+## choix un nombre d'élément dans la liste
+pokemon_show = pokemon.sample(5)
 
+## somme des colonne null
+somme = pokemon.isnull().sum()
+print(somme)
